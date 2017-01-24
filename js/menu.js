6d80cda261c67a394baa3752d2403303d27cf7bc
Max <maxim00066@gmail.com>
2017-01-24 20:39:21 +0100

    commit

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   
 	$('.menu-trigger').click(function() { 
-	 $('nav ul').slideToggle(500);
+	 $('.menu').slideToggle(500);
 	 $('.menu-trigger').toggleClass('menu-trigger-transform');
 	});
 	$(window).resize(function() {
 		if ($(window).width() >500) {
-			$('nav ul').removeAttr('style');
+			$('.menu').removeAttr('style');
 		}
 	});
 });
